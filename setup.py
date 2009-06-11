3e19f1495a00747a6a98f8e79b1b043250f90482
Sylvain Viollon <thefunny@gmail.com>
2009-06-11 12:49:29 +0000

    Spacesuits.

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.1'
+
+setup(name='silva.core.upgrade',
+      version=version,
+      description="Generic upgrade functions",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='silva core upgrade',
+      author='Infrae',
+      author_email='info@infrae.com',
+      url='',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['silva'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
