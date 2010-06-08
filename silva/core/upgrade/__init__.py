2012d85801372299c72be21f432f974317edcef2
Sylvain Viollon <thefunny@gmail.com>
2010-06-08 14:26:33 +0000

    Add a persistence.

--- silva/core/upgrade/__init__.py
+++ silva/core/upgrade/__init__.py
@@ -6,6 +6,8 @@
 CLASS_CHANGES = {
     'Products.Annotations.AnnotationTool Annotations':
         'persistent.mapping PersistentMapping',
+    'Products.Five.site.localsite FiveSiteManager':
+        'Persistence._Persistence Persistent',
     'Products.Silva.interfaces IInvisibleService':
         'silva.core.interfaces.service IInvisibleService',
     'Products.Silva.interfaces.service IInvisibleService':
