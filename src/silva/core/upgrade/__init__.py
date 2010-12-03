0c9ffedeeb1dcfebd4cbead348076ad2aeba9235
Sylvain Viollon <thefunny@gmail.com>
2010-12-03 17:07:44 +0100

    Create a 2.4 branch

--- src/silva/core/upgrade/__init__.py
+++ src/silva/core/upgrade/__init__.py
@@ -2,44 +2,44 @@
 # See also LICENSE.txt
 # $Id$
 
-import ZODB.broken
-import zope.security.proxy
-import zope.site.hooks
-import zope.site.site
-import zope.app.component.hooks
+# import ZODB.broken
+# import zope.security.proxy
+# import zope.site.hooks
+# import zope.site.site
+# import zope.app.component.hooks
 
-def setSite(site=None):
-    if site is None:
-        sm = zope.component.getGlobalSiteManager()
-    else:
+# def setSite(site=None):
+#     if site is None:
+#         sm = zope.component.getGlobalSiteManager()
+#     else:
 
-        # We remove the security proxy because there's no way for
-        # untrusted code to get at it without it being proxied again.
+#         We remove the security proxy because there's no way for
+#         untrusted code to get at it without it being proxied again.
 
-        # We should really look look at this again though, especially
-        # once site managers do less.  There's probably no good reason why
-        # they can't be proxied.  Well, except maybe for performance.
+#         We should really look look at this again though, especially
+#         once site managers do less.  There's probably no good reason why
+#         they can't be proxied.  Well, except maybe for performance.
 
-        site = zope.security.proxy.removeSecurityProxy(site)
-        sm = site.getSiteManager()
-        if isinstance(sm, ZODB.broken.Broken):
-            # If the site manager is broken don't set it.
-            return
+#         site = zope.security.proxy.removeSecurityProxy(site)
+#         sm = site.getSiteManager()
+#         if isinstance(sm, ZODB.broken.Broken):
+#             If the site manager is broken don't set it.
+#             return
 
-    zope.site.hooks.siteinfo.site = site
-    zope.site.hooks.siteinfo.sm = sm
-    try:
-        del zope.site.hooks.siteinfo.adapter_hook
-    except AttributeError:
-        pass
+#     zope.site.hooks.siteinfo.site = site
+#     zope.site.hooks.siteinfo.sm = sm
+#     try:
+#         del zope.site.hooks.siteinfo.adapter_hook
+#     except AttributeError:
+#         pass
 
-zope.site.hooks.setSite = setSite
-zope.app.component.hooks.setSite = setSite
+# zope.site.hooks.setSite = setSite
+# zope.app.component.hooks.setSite = setSite
 
-def threadSiteSubscriber(ob, event):
-    setSite(ob)
+# def threadSiteSubscriber(ob, event):
+#     setSite(ob)
 
-zope.site.site.threadSiteSubscriber = threadSiteSubscriber
+# zope.site.site.threadSiteSubscriber = threadSiteSubscriber
 
 
 CLASS_CHANGES = {
