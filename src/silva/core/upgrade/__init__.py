c3566c420cf6231f7f39fd328129620d225ee1c8
Sylvain Viollon <thefunny@gmail.com>
2012-08-24 11:28:06 +0200

    Cleanup, add a rewrite for a piece of code that moved.

--- src/silva/core/upgrade/__init__.py
+++ src/silva/core/upgrade/__init__.py
@@ -2,51 +2,14 @@
 # Copyright (c) 2009-2012 Infrae. All rights reserved.
 # See also LICENSE.txt
 
-# import ZODB.broken
-# import zope.security.proxy
-# import zope.site.hooks
-# import zope.site.site
-# import zope.app.component.hooks
-
-# def setSite(site=None):
-#     if site is None:
-#         sm = zope.component.getGlobalSiteManager()
-#     else:
-
-#         We remove the security proxy because there's no way for
-#         untrusted code to get at it without it being proxied again.
-
-#         We should really look look at this again though, especially
-#         once site managers do less.  There's probably no good reason why
-#         they can't be proxied.  Well, except maybe for performance.
-
-#         site = zope.security.proxy.removeSecurityProxy(site)
-#         sm = site.getSiteManager()
-#         if isinstance(sm, ZODB.broken.Broken):
-#             If the site manager is broken don't set it.
-#             return
-
-#     zope.site.hooks.siteinfo.site = site
-#     zope.site.hooks.siteinfo.sm = sm
-#     try:
-#         del zope.site.hooks.siteinfo.adapter_hook
-#     except AttributeError:
-#         pass
-
-# zope.site.hooks.setSite = setSite
-# zope.app.component.hooks.setSite = setSite
-
-# def threadSiteSubscriber(ob, event):
-#     setSite(ob)
-
-# zope.site.site.threadSiteSubscriber = threadSiteSubscriber
-
 
 CLASS_CHANGES = {
     'Products.Annotations.AnnotationTool Annotations':
         'persistent.mapping PersistentMapping',
     'Products.Silva.emaillinesfield EmailLinesField':
         'Products.Formulator.EmailLinesField EmailLinesField',
+    'Products.Silva.UnicodeSplitter Splitter':
+        'silva.core.services.splitter Splitter',
     'Products.Silva.interfaces IInvisibleService':
         'silva.core.interfaces.service IInvisibleService',
     'Products.Silva.interfaces.service IInvisibleService':
