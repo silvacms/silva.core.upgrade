5b5a00a2e5bd500795b2419a4fbc961d7885a864
Sylvain Viollon <thefunny@gmail.com>
2013-10-09 16:37:39 +0200

    Tag 3.0.1.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2012  Infrae. All rights reserved.
+# Copyright (c) 2012-2013 Infrae. All rights reserved.
 # See also LICENSE.txt
 from setuptools import setup, find_packages
 import os
 
-version = '3.0.1dev'
+version = '3.0.1'
 
 tests_require = [
     'Products.Silva [test]',
--- src/silva/core/upgrade/upgrader/upgrade_303.py
+++ src/silva/core/upgrade/upgrader/upgrade_303.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2013  Infrae. All rights reserved.
+# See also LICENSE.txt
 from silva.core.upgrade.upgrade import BaseUpgrader
 from silva.core.editor.utils import DEFAULT_PER_TAG_WHITELISTS
 from silva.core.editor.utils import DEFAULT_HTML_ATTR_WHITELIST
